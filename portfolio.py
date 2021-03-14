a83a9d303cf1181012795cf3f6d10886a414c590
Caroline Tao <60095297+caroljtao@users.noreply.github.com>
2021-03-14 19:57:37 -0400

    Update portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -1,4 +1,7 @@
 stocks=['JNJ','AAPL','SLF-PC.TO','QCOM']            #johnson and johnson, apple, sunlife financial, quanlcomm
 portfolio=pd.DataFrame()
 for i in stocks:                   
-    portfolio(i)=wb.DataReader(i,data_source="yahoo",start="2020-09-01")  (cannot aasign function call -- check it out tmr)
+    portfolio[i]=wb.DataReader(i,data_source="yahoo",start="2020-09-01")['Adj Close']
+portfolio.head() 
+
+
