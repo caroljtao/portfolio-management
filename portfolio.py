1dee96c80113f4dbb46b8d76569dec169e221773
Caroline Tao <60095297+caroljtao@users.noreply.github.com>
2021-03-15 10:35:41 -0400

    Update portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -10,3 +10,5 @@ log_return=np.log(portfolio/portfolio.shift(1)).plot(figsize=(10,6))
 weight=[0.3,0.4,0.1,0.2]
 np.dot(discrete_return,weight)             #return a matrix that contains the portfolo return on each day, can't be right. Gotta average the return of each stock and then dot
 portfolio return = np.dot(discrete_return.mean()*250,weight)
+# covariance calculation in the following
+discrete_return.cov()
