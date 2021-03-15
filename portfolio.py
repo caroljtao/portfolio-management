85784e1e22355a263ec9df89ae3f933e0240092e
Caroline Tao <60095297+caroljtao@users.noreply.github.com>
2021-03-14 21:25:56 -0400

    Update portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -3,5 +3,7 @@ portfolio=pd.DataFrame()
 for i in stocks:                   
     portfolio[i]=wb.DataReader(i,data_source="yahoo",start="2020-09-01")['Adj Close']
 portfolio.head() 
-
-
+portfolio.iloc[0][1]
+(portfolio/portfolio.iloc[0]*100).plot(figsize=(10,6)              #normalize each stock's return and plot it
+(discrete_return=portfolio/portfolio.shift(1)-1).plot(figsize=（10,6))
+log_return=np.log(portfolio/portfolio.shift(1)).plot(figsize=(10,6))
