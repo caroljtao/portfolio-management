7ce0cdd1acbd83bb276ba1946e06c22b5d4a5881
Caroline Tao <60095297+caroljtao@users.noreply.github.com>
2021-04-02 13:36:27 -0400

    Update monte carlo simulation.py

--- monte carlo simulation.py
+++ monte carlo simulation.py
@@ -23,6 +23,7 @@ gross_profit.std()
 plt.figure(figsize=(15,6))
 plt.hist(gross_profit,bins=20)                 #draw the histogram to show the distribution of gross profits => find it's a normal distribution
 
+###
 # asset pricing with monte carlo simulation
 # use the formula for asset pricing: p(Today)=p(yesterday)*e^r, r is the log return of share price between yesterday and toda
 # use Brownian motion to model r (r is a random variable)
@@ -32,7 +33,33 @@ plt.hist(gross_profit,bins=20)                 #draw the histogram to show the d
 from scipy.stats import norm
 from pandas_datareader import data as wb
 %matplotlib inline
-JNJ=wb.DataReader("JNJ",data_source="yahoo",start="2017-09-01")
+data=pd.DataFrame()
+ticker="JNJ"
+data[ticker]=wb.DataReader("JNJ",data_source="yahoo",start="2010-01-01")['Adj Close']
+data[ticker]             #Pay attention to the last 4 lines.
 logreturn=np.log(1+JNJ.pct_change())
 plt.figure(figsize=(10,6))
-plt.plot(logreturn)
+plt.plot(logreturn)       #has a stable mean and normally distributed 
+u=logreturn.mean()
+drift=u-0.5*logreturn.var()
+np.array(drift)
+drift.values
+x=np.random.rand(10,2)
+z_score=norm.pff(x)
+time_intervals=500      #predict the prices for the next 500 days
+iterations=50
+
+np.array(drift)
+returns=np.exp(drift.values+stock_stdd.values*norm.ppf(np.random.rand(time_intervals,iterations)))
+returns
+S0=data.iloc[-1]
+price_list=np.zeros_like(returns)
+price_list[0]=S0
+for t in range(1, time_intervals):
+  price_list[t]=price_list[t-1]*returns[t]
+
+plt.figure(figsize=(15,6))
+plt.plot(price_list)
+
+
+
