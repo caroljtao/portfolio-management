08a28bab07e0e9d24699a244f3bd4a4a93673176
Caroline Tao <60095297+caroljtao@users.noreply.github.com>
2021-04-01 21:40:59 -0400

    Update regression analysis.py

--- regression analysis.py
+++ regression analysis.py
@@ -5,3 +5,6 @@ import statsmodels.api as sm
 import matplotlib.pyplot as plt
 JNJ_regression_data=pd.read_excel("C:/Users/Caroline Tao/Desktop/JNJ.xlsx")
 JNJ_regression_data.head()
+Y=JNJ_regression_data['Adj Close']
+X=JNJ_regression_data['Date']
+Y.head(),X.head()
