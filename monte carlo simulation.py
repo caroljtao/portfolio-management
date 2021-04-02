c5e8d1edab231f4adf9673fa535aa4987cdb65d0
Caroline Tao <60095297+caroljtao@users.noreply.github.com>
2021-04-02 10:50:50 -0400

    Update monte carlo simulation.py

--- monte carlo simulation.py
+++ monte carlo simulation.py
@@ -4,3 +4,9 @@ rev_mean=100
 rev_stdd=20
 iterations=1000
 rev=np.random.normal(rev_mean,rev_stdd,iterations)
+plt.figure(figsize=(10,6))
+plt.plot(rev)          #shows that the mean value floats around 100
+COGS=-(rev*np.random.normal(0.6,0.1))    #COGS equals 60% of the revenue and the stdd= 10%
+plt.figure(figsize=(10,6))
+COGS.mean()
+COGS.std()
