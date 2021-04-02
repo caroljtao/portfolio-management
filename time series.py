f6efaedea651c8961ea8f4bf19306a36f483ba44
Caroline Tao <60095297+caroljtao@users.noreply.github.com>
2021-04-01 22:32:42 -0400

    Update time series.py

--- time series.py
+++ time series.py
@@ -14,4 +14,6 @@ plt.ylabel('price')
 # Time series here X needs to be converted to float int64. Date can't be used to run regression. 
 constant=sm.add_constnat(X)
 reg=sm.OLS(Y,constant).fit()               #turn X into float int64
-
+slope,intercept,r,p_value,stdd_error=stats.linregress(X,Y)
+slope
+p_value
