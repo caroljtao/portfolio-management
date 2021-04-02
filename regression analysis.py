508e5f9fae27b1021b2108085a38f94604e033d7
Caroline Tao <60095297+caroljtao@users.noreply.github.com>
2021-04-01 21:38:30 -0400

    Update regression analysis.py

--- regression analysis.py
+++ regression analysis.py
@@ -3,3 +3,5 @@ import pandas as pd
 from scipy import stats
 import statsmodels.api as sm
 import matplotlib.pyplot as plt
+JNJ_regression_data=pd.read_excel("C:/Users/Caroline Tao/Desktop/JNJ.xlsx")
+JNJ_regression_data.head()
